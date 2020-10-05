4a31ef2256708ba5329ba00e27bafd0573190fb9
Amman Sood <392075+asood92@users.noreply.github.com>
2020-10-05 17:35:15 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # poetry_slam
- 
+
+# This program reads in a poem from a text file and prints it backwards, then randomly, and lastly prints the original line only if it is an odd numbered line - otherwise it prints a random line
--- poetry_slam.py
+++ poetry_slam.py
@@ -1,4 +1,3 @@
-# This program reads in a poem from a text file and prints it backwards, then randomly, and lastly prints the original line only if it is an odd numbered line - otherwise it prints a random line
 import random
 
 #  file IO for list manipulation
